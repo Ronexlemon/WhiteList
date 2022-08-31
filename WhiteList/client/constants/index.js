e79f9e96c1f08dcba7b050744883058939cae969
RonexLemon <ronexondimu@gmail.com>
2022-08-31 04:56:22 +0300

    intergrate hardhat to client

---
+++ WhiteList/client/constants/index.js
@@ -0,0 +1,69 @@
+export const WHITELIST_CONTRACT_ADDRESS = "0x71ec1768B5962c638d3a787115AaA1D9B01119e0";
+export const abi = 
+     [
+      {
+        "inputs": [
+          {
+            "internalType": "uint8",
+            "name": "_maxWhitelistedAddresses",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [],
+        "name": "addAddressToWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "maxWhitelistedAddresses",
+        "outputs": [
+          {
+            "internalType": "uint8",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "numAddressesWhitelisted",
+        "outputs": [
+          {
+            "internalType": "uint8",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "name": "whitelistedAddresses",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]
+    
+  ;
